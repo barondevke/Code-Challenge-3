a6c3e35d12051156f55fe89ff67e1518cd079198
Steve Baron <ndabasteve1@gmail.com>
2023-07-02 13:15:08 +0300

    Changed createFirstMovie function to showAllMovies

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 fetch(' http://localhost:3000/films/1')
     .then((res) => res.json())
-    .then((json) => createFirstMovie(json))
+    .then((json) => showAllMovie(json))
 
 
 
-function createFirstMovie(data) {
-    let movieTitle = document.createElement('h2')
-    movieTitle.innerText = data.title
+function showAllMovies(data) {
+    data.forEach(element => {
+        let movieTitle = document.createElement('h2')
+        movieTitle.innerText = element.title
 
-    let poster = document.createElement('img')
-    poster.src = data.poster
+        let poster = document.createElement('img')
+        poster.src = element.poster
 
-    let movieRuntime = document.createElement('h3')
-    movieRuntime.innerText = `${data.runtime} minutes`
+        let movieRuntime = document.createElement('h3')
+        movieRuntime.innerText = `${element.runtime} minutes`
 
-    let movieShowtime = document.createElement('h3')
-    movieShowtime.innerText = data.showtime
+        let movieShowtime = document.createElement('h3')
+        movieShowtime.innerText = element.showtime
 
-    let availableMovieTickets = data.capacity - data.tickets_sold
-    let movieTickets = document.createElement('h4')
-    movieTickets.innerHTML = `Tickets left : ${availableMovieTickets}`
+        let availableMovieTickets = element.capacity - element.tickets_sold
+        let movieTickets = document.createElement('h4')
+        movieTickets.innerHTML = `Tickets left : ${availableMovieTickets}`
 
-    let buyBtn = document.createElement('button')
-    buyBtn.innerHTML = 'Buy Ticket'
-    buyBtn.onclick = () => {
-        let availableMovieTickets = data.capacity - data.tickets_sold
-        if (availableMovieTickets >= 1) {
-            availableMovieTickets - 1
-            movieTickets.innerHTML = `Tickets left : ${availableMovieTickets}`
+        let buyBtn = document.createElement('button')
+        buyBtn.innerHTML = 'Buy Ticket'
+
+
+        const moreDetails = document.createElement('button')
+        moreDetails.onclick = () => {
+            document.getElementById('Firstmovie').appendChild(movieRuntime)
+            document.getElementById('Firstmovie').appendChild(movieShowtime)
+            document.getElementById('Firstmovie').appendChild(movieTickets)
 
         }
 
-        else {
-            movieTickets.innerHTML = 'Sold Out'
-        }
-    }
-
-    document.getElementById('Firstmovie').appendChild(movieTitle)
-    document.getElementById('Firstmovie').appendChild(poster)
-    document.getElementById('Firstmovie').appendChild(movieRuntime)
-    document.getElementById('Firstmovie').appendChild(movieShowtime)
-    document.getElementById('Firstmovie').appendChild(movieTickets)
-    document.getElementById('Firstmovie').appendChild(buyBtn)
+        document.getElementById('Firstmovie').appendChild(movieTitle)
+        document.getElementById('Firstmovie').appendChild(poster)
+        document.getElementById('Firstmovie').appendChild(moreDetails)
 
 
+        document.getElementById('Firstmovie').appendChild(buyBtn)
+
+
+
+
+    });
 
 
 }
